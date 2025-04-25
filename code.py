4a649502ecdd586de1171ea24dd3e3f499a56ac5
Bibek kushwaha <bibekkushwaha719@gmail.com>
2025-04-25 17:44:23 +0545

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+#It's for finding positive or negative number 
+
+print ("Hello, Dcoder!")
+number = float(input("Enter your number:"))
+if number>0:{
+  print(f"{number} is a positive number 😁")
+}
+elif number==0:{
+  print("It's Zero 0️⃣")
+}
+else:{
+  print(f"{number} is a neagtive number 😃")
+}
